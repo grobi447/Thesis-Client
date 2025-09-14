9c9632e54b1f923a5b08bf522cc8327857268234
Robert Gaspar <robert.gaspar@oracle.com>
2025-09-14 22:32:19 +0200

    Spike animation

--- Assets/Scripts/MapEditor/SpikeTile.cs
+++ Assets/Scripts/MapEditor/SpikeTile.cs
@@ -1,8 +1,53 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpikeTile : Tile
 {
-    
+    private Animator animator;
+    public float animationFirstStartDelay;
+    public float animationStartDelay;
+    public float idleTime;
+    private Coroutine animationCoroutine;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animationFirstStartDelay = 0f;
+        animationStartDelay = 1f;
+        idleTime = 1f;
+    }
+
+    void Start()
+    {
+        animationCoroutine = StartCoroutine(PlayAnimationSequence());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestartTimer();
+        }
+    }
+
+    public void RestartTimer()
+    {
+        if (animationCoroutine != null) {
+            animator.SetTrigger("triggerOff");
+            StopCoroutine(animationCoroutine);
+        }
+        animationCoroutine = StartCoroutine(PlayAnimationSequence());
+    }
+
+    private IEnumerator PlayAnimationSequence()
+    {
+        yield return new WaitForSeconds(animationFirstStartDelay);
+        while (true)
+        {
+            animator.SetTrigger("triggerIn");
+            yield return new WaitForSeconds(idleTime);
+            animator.SetTrigger("triggerOut");
+            yield return new WaitForSeconds(animationStartDelay);
+        }
+    }
 }
